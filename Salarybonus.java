be399f7c9c934d8a24be6fda52a7f12665d2ef90
indrareddy333 <reddyindra843@gmail.com>
2024-11-29 05:36:00 +0530

    Practice Problem - To calaculate bonus according to salary

---
+++ Salarybonus.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Salarybonus {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your slaray to get a bonus : ");
+        int num = scan.nextInt();
+        if (num < 20000) {
+            System.out.println("Bonus amount : " + 1500);
+            System.out.println("Total amount : " + (num + 1500));
+        } else {
+            System.out.println("Bonus Amount : " + 2500);
+            System.out.println("Total amount : " + (num + 2500));
+        }
+    }
+}
