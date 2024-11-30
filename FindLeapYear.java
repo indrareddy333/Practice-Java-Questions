e0b675a365c7b3332ee3931d18ce5ba437672b6b
indrareddy333 <reddyindra843@gmail.com>
2024-11-30 08:49:45 +0530

    Practice problem - Find a Leap year or Not

---
+++ FindLeapYear.java
@@ -0,0 +1,11 @@
+public class FindLeapYear {
+    public static void main(String[] args) {
+        int year = 2016;
+        if (year % 4 == 0) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+
+}
