9bcead3776a961e6911bad7e4459e70358ed56f3
indrareddy333 <reddyindra843@gmail.com>
2024-12-03 05:16:09 +0530

    practice problem - Take a number as input and print the multiplication table for it.

---
+++ Printing_table.java
@@ -0,0 +1,14 @@
+//Take a number as input and print the multiplication table for it.
+
+import java.util.Scanner;
+
+public class Printing_table {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a number you want to print whole multiplication table = ");
+        int num = scan.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+}
