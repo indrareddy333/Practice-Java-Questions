e992c028d20f5fc8aadd0e42ec877308b220296b
indrareddy333 <reddyindra843@gmail.com>
2024-12-05 05:45:33 +0530

    Practice Problem - Primitives in java

---
+++ Primitives.java
@@ -0,0 +1,14 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 45;
+        char letter = 'r';
+        float marks = 34.4f;
+        double largedecimal = 34.938349;
+        long longnum = 342342352351235134L;
+        boolean cheak = false;
+
+        System.out.println(rollno + ", " + letter + ", " + marks + ", " + largedecimal + ", " + longnum + ", " + cheak);
+
+    }
+
+}
