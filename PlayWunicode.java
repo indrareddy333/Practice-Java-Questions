03087af01908cbf46b9777d25bbff785de1bbe14
indrareddy333 <reddyindra843@gmail.com>
2024-12-06 06:58:19 +0530

    Practice Problem - Printing local lang in java

---
+++ PlayWunicode.java
@@ -0,0 +1,13 @@
+﻿// printing unicode lang which UTF_16 
+
+import java.util.Scanner;
+
+public class PlayWunicode {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+
+        System.out.println("नमस्ते" + name + "! ఎలా ఉన్నారు");
+        scan.close();
+    }
+}
